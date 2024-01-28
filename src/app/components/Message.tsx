a93adbdc0f430e997a5e7f7016532b46118d9a5d
CahidArda <cahidardaooz@hotmail.com>
2024-01-28 14:57:13 +0300

    update degreeguru ui

---
+++ src/app/components/Message.tsx
@@ -0,0 +1,25 @@
+import Markdown from 'markdown-to-jsx'
+
+interface MessageProps {
+  message: {
+    role: string;
+    content: string;
+  };
+}
+
+const Message: React.FC<MessageProps> = ({ message }) => {
+  const containerClass = message.role === 'user' ? 'bg-[#e7e7e9] text-[##FAFAFA]' : 'bg-[#F8F8F8] text-[#43403B]';
+  const roleText = message.role === 'user' ? 'Question' : 'DegreeGuru';
+
+  return (
+    <div className={`p-2 px-5 mb-1 ${containerClass}`}>
+      <div>
+        <Markdown>
+          {`**${roleText}:** ${message.content}`}
+        </Markdown>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,47 @@
 "use client"
 
 import { useChat } from 'ai/react';
-import Markdown from 'markdown-to-jsx'
-
-import Header from "./components/Header";
-
-interface MyData {
-  output: string;
-  sources: string[];
-}
+import Message from './components/Message';
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: "api/guru"
+    api: "/api/guru"
   });
-
   return (
-    <div className="flex flex-col h-screen">
-      <Header />
-      <div className="flex flex-col items-center justify-center flex-grow p-4">
-        {messages.map((m) => {
-          let contentToRender;
-
-          if (m.role === 'user') {
-            contentToRender = `User: ${m.content}`;
-          } else if (m.content.includes("_no_streaming_response_")) {
-            const parsedContent: MyData = JSON.parse(m.content);
-            const setsources = Array.from(new Set(parsedContent.sources).values());
-            contentToRender = `AI: ${parsedContent.output}\n\nSources:\n${setsources
-              .map((url: string) => `- ${url}\n`)
-              .join('')}`;
-          } else {
-            contentToRender = `AI: ${m.content}`;
+    <div className="p:2 flex h-screen flex-col items-center justify-between bg-[#F0F0F0]">
+      <div className="mb-1 w-full bg-[#FFFFFF] p-4 px-4 pt-4">
+        <h1 className="text-lg font-semibold">DegreeGuru</h1>
+      </div>
+      <div className="w-1/2 flex flex-col justify-between items-center flex-grow">
+        <div
+          id="messages"
+          className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex flex-col overflow-y-auto"
+        >
+          {
+            messages.map(m => (<Message message={m}/>))
           }
-
-          return (
-            <div id="message-box" key={m.id} className="whitespace-pre-wrap max-w-3xl my-4">
-              <Markdown>
-                {contentToRender}
-              </Markdown>
-            </div>
-          );
-        })}
-
-        <div className="w-full max-w-md">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={input}
-              onChange={handleInputChange}
-              placeholder="Enter text"
-              className="w-full bg-black text-white p-2 rounded-none border-none"
-            />
-          </form>
+        </div>
+        <div className="border-t-2 border-gray-200 w-full relative flex">
+            <form
+              onSubmit={handleSubmit}
+              className="m-auto flex w-full max-w-screen-lg items-center justify-center space-x-4 p-4"
+            >
+              <input
+                id="message"
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+                x-model="newMessage"
+                placeholder="Your message..."
+                className="flex-1 rounded-md border border-gray-300 p-2"  
+              />
+              <button className="rounded-md bg-gray-800 px-4 py-2 text-white">
+                Send
+              </button>
+            </form>
         </div>
       </div>
     </div>
-
+    
   );
 }
