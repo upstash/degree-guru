e9de3d22e2a092f9ab4f50af3658a7525b4bd962
CahidArda <cahidardaooz@hotmail.com>
2024-02-20 20:32:30 +0300

    add user icons and fix text field to the bottom

--- src/app/components/Message.tsx
+++ src/app/components/Message.tsx
@@ -9,12 +9,14 @@ interface MessageProps {
 
 const Message: React.FC<MessageProps> = ({ message }) => {
   const containerClass = message.role === 'user' ? 'bg-[#e7e7e9] text-[##FAFAFA]' : 'bg-[#F8F8F8] text-[#43403B]';
-  const roleText = message.role === 'user' ? 'Question' : 'DegreeGuru';
+  const iconClass = message.role === 'user' ? 'h-7 w-7' : 'h-6 w-6 mr-3 m-1'
+  const icon = message.role === 'user' ? '/icon-user.png' : '/favicon.ico';
 
   return (
-    <div className={`p-2 px-5 border-b text-sm ${containerClass}`}>
-      <Markdown className={"space-y-2"}>
-        {`**${roleText}:** ${message.content}`}
+    <div className={`flex items-start p-2 border-b text-sm w-full ${containerClass}`}>
+      <img src={icon} className={`${iconClass} mx-2`} alt="Icon" />
+      <Markdown className={"pr-9 py-1 space-y-2"}>
+        {message.content}
       </Markdown>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link'
 import { useChat } from 'ai/react';
 import Message from './components/Message';
@@ -18,7 +18,7 @@ export default function Home() {
     streaming: false
   });
 
-  const { messages, input, handleInputChange, handleSubmit, setMessages } = useChat({
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/guru",
     onFinish: () => {
       setSelectedOption(prevState => ({
@@ -28,9 +28,18 @@ export default function Home() {
     }
   });
 
+  // move view to the bottom of the messages when messages change
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
-    <div className="p:2 flex h-screen flex-col items-center justify-between bg-[#F0F0F0]">
-      <div className="w-full h-1/10 bg-[#FFFFFF] pr-40 border-b flex flex-row justify-between">
+    <div className="p:2 flex h-screen flex-col items-center bg-[#F0F0F0]">
+
+      <div className="h-[7.5vh] w-full bg-[#FFFFFF] pr-40 border-b flex flex-row justify-between">
         <div className="p-4">
           <h1 >
             <Link className="text-xl font-semibold text-[#43403B]" href="/">
@@ -54,50 +63,48 @@ export default function Home() {
           {/* empty div */}
         </div>
       </div>
-      <div className="transition-all w-3/4 lg:w-1/2 h-9/10 flex flex-col justify-between items-center flex-grow overflow-y-auto border-x">
-        <div
-          id="messages" 
-          className="transition scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex flex-col w-full"
-        >
-          <Landing/>
-          {
-            messages.map(m => (<Message message={m} key={m.id}/>))
-          }
-        </div>
-        <div className="text-sm border-t border-gray-200 w-full relative">
-            <form
-              onSubmit={e => {
-                handleSubmit(e, {
-                  data: {
-                    vectorStore: state.selected
-                  },
-                });
-                setSelectedOption(prevState => ({
-                  ...prevState,
-                  showLanding: false,
-                  streaming: true
-                }));
-              }}
-              className="m-auto flex w-full max-w-screen-lg items-center justify-center space-x-4 p-4"
+      
+      <div className="h-[84vh] transition-all w-3/4 lg:w-1/2 flex flex-col items-center border-x scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-1 overflow-y-scroll">
+        <Landing/>
+        {
+          messages.map(m => (<Message message={m} key={m.id}/>))
+        }
+        <div ref={messagesEndRef} />
+      </div>
+
+      <div className="h-[8.5vh] absolute bottom-0 lg:left-1/4 w-3/4 lg:w-1/2 text-sm border border-gray-200">
+          <form
+            onSubmit={e => {
+              handleSubmit(e, {
+                data: {
+                  vectorStore: state.selected
+                },
+              });
+              setSelectedOption(prevState => ({
+                ...prevState,
+                showLanding: false,
+                streaming: true
+              }));
+            }}
+            className="m-auto flex items-center justify-center space-x-4 p-4"
+          >
+            <input
+              id="message"
+              type="text"
+              disabled={state.streaming}
+              value={input}
+              onChange={handleInputChange}
+              x-model="newMessage"
+              placeholder="Your question..."
+              className="transition flex-1 rounded-md border border-gray-300 p-2"  
+            />
+            <button
+              className="transition rounded-md bg-gray-800 px-4 py-2 text-[#F0F0F0] disabled:bg-gray-300"
+              disabled={state.streaming}
             >
-              <input
-                id="message"
-                type="text"
-                disabled={state.streaming}
-                value={input}
-                onChange={handleInputChange}
-                x-model="newMessage"
-                placeholder="Your question..."
-                className="transition flex-1 rounded-md border border-gray-300 p-2"  
-              />
-              <button
-                className="transition rounded-md bg-gray-800 px-4 py-2 text-[#F0F0F0] disabled:bg-gray-300"
-                disabled={state.streaming}
-              >
-                Send
-              </button>
-            </form>
-        </div>
+              Send
+            </button>
+          </form>
       </div>
     </div>
     
