dc8f0aff66cb31d888bb2a69941262b4bdcf5791
CahidArda <cahidardaooz@hotmail.com>
2024-02-23 19:54:58 +0300

    add docstrings

--- degreegurucrawler/degreegurucrawler/spiders/configurable.py
+++ degreegurucrawler/degreegurucrawler/spiders/configurable.py
@@ -1,23 +1,49 @@
 
+import os
 import uuid
 import logging
+
+from ..utils.upstash_vector_store import UpstashVectorStore
+from ..utils.config import text_splitter_config, crawler_config
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from ..utils.config import config, text_splitter_config
-from ..utils.db import collection
-
-text_splitter = RecursiveCharacterTextSplitter(**text_splitter_config)
-
 
 class ConfigurableCrawler(CrawlSpider):
 
+    name = "configurable"
+    start_urls = crawler_config["start_urls"]
+    rules = (
+        Rule(
+            LinkExtractor(
+                **crawler_config["link_extractor"]
+            ),
+        callback="parse_page",
+        follow=True # to enable following links on each page when callback is provided
+        ),
+    )
+
     def __init__(self, *a, **kw):
         super().__init__(*a, **kw)
-        
+
+        self.vectorstore = UpstashVectorStore(
+            url=os.environ["UPSTASH_VECTOR_REST_URL"],
+            token=os.environ["UPSTASH_VECTOR_REST_TOKEN"]
+        )
+
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            **text_splitter_config
+        )
+
+
+    def _disable_loggers(self):
+        """
+        disables some of the loggers to keep the log clean
+        """
+
         disable_loggers = [
             "scrapy.spidermiddlewares.depth",
             "protego",
@@ -29,29 +55,27 @@ class ConfigurableCrawler(CrawlSpider):
         for logger in disable_loggers:
             logging.getLogger(logger).setLevel(logging.WARNING)
 
-    name = "configurable"
-    start_urls = config["crawler"]["start_urls"]
-    rules = (Rule(
-        LinkExtractor(
-            **config["crawler"]["link_extractor"]
-        ),
-        callback="parse_page",
-        follow=True # to enable following links on each page when callback is provided
-    ),)
-
     def parse_page(self, response):
+        """
+        Creates chunks out of the crawled webpage and adds them to the vector
+        store.
+        """
 
         # extract text content
         text_content = response.xpath('//p').getall()
         text_content = '\n'.join(text_content)
 
-        link = response.url
-        documents = text_splitter.split_text(text_content)
+        # split documents
+        documents = self.text_splitter.split_text(text_content)
 
         if len(documents) == 0:
             return
 
-        collection.add(
+        # get source url
+        link = response.url
+
+        # add documents to vector store
+        self.vectorstore.add(
             ids=[str(uuid.uuid4())[:8] for doc in documents],
             documents=documents,
             link=link
