c42364f1bda3967fd83da15c778a15a67fe4ff36
CahidArda <cahidardaooz@hotmail.com>
2024-02-23 19:54:33 +0300

    rename db.py as upstash_vector_store.py

---
+++ degreegurucrawler/degreegurucrawler/utils/upstash_vector_store.py
@@ -0,0 +1,54 @@
+from typing import List
+from openai import OpenAI
+from upstash_vector import Index
+
+class UpstashVectorStore:
+
+    def __init__(
+            self,
+            url: str,
+            token: str
+    ):
+        self.client = OpenAI()
+        self.index = Index(url=url, token=token)
+        import pdb; pdb.set_trace()
+
+    def get_embeddings(
+            self,
+            documents: List[str],
+            model: str = "text-embedding-ada-002"
+    ) -> List[List[float]]:
+        """
+        Given a list of documents, generates and returns a list of embeddings
+        """
+        documents = [document.replace("\n", " ") for document in documents]
+        embeddings = self.client.embeddings.create(
+            input = documents,
+            model=model
+        )
+        return [data.embedding for data in embeddings.data]
+
+    def add(
+            self,
+            ids: List[str],
+            documents: List[str],
+            link: str
+    ) -> None:
+        """
+        Adds a list of documents to the Upstash Vector Store
+        """
+        embeddings = self.get_embeddings(documents)
+        self.index.upsert(
+            vectors=[
+                (
+                    id,
+                    embedding,
+                    {
+                        "text": document,
+                        "url": link
+                    }
+                )
+                for id, embedding, document
+                in zip(ids, embeddings, documents)
+            ]
+        )
