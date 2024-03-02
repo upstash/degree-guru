d7869a6bc1491370a2b4bb43afb0f6360dfb32d4
CahidArda <cahidardaooz@hotmail.com>
2024-03-02 11:00:25 +0300

    change master deadline question to courses on AI

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ export const INITIAL_QUESTIONS = [
     content: "What program can I study if I am interested in aviation?",
   },
   {
-    content: "What is the application deadline for the master's program?",
+    content: "What courses are available for someone interested in AI?",
   },
   {
     content: "What courses will I take as a computer engineering student?",
