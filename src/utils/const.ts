86bbcf02452701d689648bae0e41917ef01b2449
CahidArda <cahidardaooz@hotmail.com>
2024-03-03 20:23:09 +0300

    change AI courses question to environmental sustainiability

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ export const INITIAL_QUESTIONS = [
     content: "What program can I study if I am interested in aviation?",
   },
   {
-    content: "What courses are available for someone interested in AI?",
+    content: "Are there courses on environmental sustainability?",
   },
   {
     content: "What courses will I take as a computer engineering student?",
