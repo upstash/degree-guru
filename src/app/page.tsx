6dc283f26df812abf7f598b07ec2a23c3d36fd71
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-19 10:31:55 +0300

    fix: remove additional scrollbar in chat area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ Your ultimate companion in navigating the academic landscape of Stanford.`,
   }, [messages]);
 
   return (
-    <main className="relative max-w-screen-md p-4 md:p-6 mx-auto flex min-h-svh !pb-32 md:!pb-40 overflow-y-scroll">
+    <main className="relative max-w-screen-md p-4 md:p-6 mx-auto flex min-h-svh !pb-32 md:!pb-40 overflow-y-auto">
       <div className="w-full">
         {messages.map((message: MessageProps) => {
           return <Message key={message.id} {...message} />;
